096103e2d18db9467cfd51f18982a84035011b79
[Tarun Goel] <[tarun.goel@castler.com]>
2025-02-20 12:19:27 +0530

    Refactor theme management to use Redux and update theme styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import RouteConfig from "./RouteConfig";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import { setTheme } from "./Redux/themeSlice";
+import { useDispatch } from "react-redux";
 const App: React.FC = () => {
   console.log("In app.tsx");
+  const dispatch = useDispatch();
   const theme = useSelector((state: any) => state.theme.theme);
+  console.log(theme);
 
   useEffect(() => {
-    document.documentElement.className = theme; // Apply theme class to <html>
+    console.log("In useffect " + theme);
+    dispatch(setTheme(theme));
+    // document.documentElement.className = theme; // Apply theme class to <html>
   }, []);
 
   return <RouteConfig />;
--- src/Redux/themeSlice.tsx
+++ src/Redux/themeSlice.tsx
@@ -1,12 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialTheme = localStorage.getItem("theme") || "idfc";
+console.log("Initial theme: " + initialTheme);
 
 const themeSlice = createSlice({
   name: "theme",
   initialState: { theme: initialTheme },
   reducers: {
     setTheme: (state, action) => {
+      console.log("In setTheme in slice");
       state.theme = action.payload;
       localStorage.setItem("theme", action.payload);
       document.documentElement.setAttribute("data-theme", action.payload);
